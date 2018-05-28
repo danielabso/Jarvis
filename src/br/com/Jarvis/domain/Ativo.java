8ea92dc4f7362dbca19523d46994d722c961d531
danielabso <37552044+danielabso@users.noreply.github.com>
2018-05-28 16:25:43 -0300

    Implementando ligação com as tabelasv2.

--- src/br/com/Jarvis/domain/Ativo.java
+++ src/br/com/Jarvis/domain/Ativo.java
@@ -1,5 +1,221 @@
 package br.com.Jarvis.domain;
 
 public class Ativo {
+	private Long idativo;
+	private String modelo;
+	private String serialouserie;
+	private String criadopor;
+	private String criadoem;
+	private String atualizadopor;
+	private String atualizadoem;
+	private String estoque;
+	private String situacao;
+	private String disponibilizadopor;
+	private String disponibilizadoem;
+	private String memoria;
+	private Long imei;
+	private String numerodanota;
+	private String discorigido;
+	private String polegadasdatela;
+	private String sistemaoperacional;
+	private String processador;
+	private String modeloprocessador;
+	private String centrodecusto;
+	private String disponivel;
+	private Fabricante idfabrincante;
+	private Linhas idlinhas;
+	private Pessoa idpessoa;
+
+	public Long getIdativo() {
+		return idativo;
+	}
+
+	public void setIdativo(Long idativo) {
+		this.idativo = idativo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getSerialouserie() {
+		return serialouserie;
+	}
+
+	public void setSerialouserie(String serialouserie) {
+		this.serialouserie = serialouserie;
+	}
+
+	public String getCriadopor() {
+		return criadopor;
+	}
+
+	public void setCriadopor(String criadopor) {
+		this.criadopor = criadopor;
+	}
+
+	public String getCriadoem() {
+		return criadoem;
+	}
+
+	public void setCriadoem(String criadoem) {
+		this.criadoem = criadoem;
+	}
+
+	public String getAtualizadopor() {
+		return atualizadopor;
+	}
+
+	public void setAtualizadopor(String atualizadopor) {
+		this.atualizadopor = atualizadopor;
+	}
+
+	public String getAtualizadoem() {
+		return atualizadoem;
+	}
+
+	public void setAtualizadoem(String atualizadoem) {
+		this.atualizadoem = atualizadoem;
+	}
+
+	public String getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(String estoque) {
+		this.estoque = estoque;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+	public String getDisponibilizadopor() {
+		return disponibilizadopor;
+	}
+
+	public void setDisponibilizadopor(String disponibilizadopor) {
+		this.disponibilizadopor = disponibilizadopor;
+	}
+
+	public String getDisponibilizadoem() {
+		return disponibilizadoem;
+	}
+
+	public void setDisponibilizadoem(String disponibilizadoem) {
+		this.disponibilizadoem = disponibilizadoem;
+	}
+
+	public String getMemoria() {
+		return memoria;
+	}
+
+	public void setMemoria(String memoria) {
+		this.memoria = memoria;
+	}
+
+	public Long getImei() {
+		return imei;
+	}
+
+	public void setImei(Long imei) {
+		this.imei = imei;
+	}
+
+	public String getNumerodanota() {
+		return numerodanota;
+	}
+
+	public void setNumerodanota(String numerodanota) {
+		this.numerodanota = numerodanota;
+	}
+
+	public String getDiscorigido() {
+		return discorigido;
+	}
+
+	public void setDiscorigido(String discorigido) {
+		this.discorigido = discorigido;
+	}
+
+	public String getPolegadasdatela() {
+		return polegadasdatela;
+	}
+
+	public void setPolegadasdatela(String polegadasdatela) {
+		this.polegadasdatela = polegadasdatela;
+	}
+
+	public String getSistemaoperacional() {
+		return sistemaoperacional;
+	}
+
+	public void setSistemaoperacional(String sistemaoperacional) {
+		this.sistemaoperacional = sistemaoperacional;
+	}
+
+	public String getProcessador() {
+		return processador;
+	}
+
+	public void setProcessador(String processador) {
+		this.processador = processador;
+	}
+
+	public String getModeloprocessador() {
+		return modeloprocessador;
+	}
+
+	public void setModeloprocessador(String modeloprocessador) {
+		this.modeloprocessador = modeloprocessador;
+	}
+
+	public String getCentrodecusto() {
+		return centrodecusto;
+	}
+
+	public void setCentrodecusto(String centrodecusto) {
+		this.centrodecusto = centrodecusto;
+	}
+
+	public String getDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(String disponivel) {
+		this.disponivel = disponivel;
+	}
+
+	public Fabricante getIdfabrincante() {
+		return idfabrincante;
+	}
+
+	public void setIdfabrincante(Fabricante idfabrincante) {
+		this.idfabrincante = idfabrincante;
+	}
+
+	public Linhas getIdlinhas() {
+		return idlinhas;
+	}
+
+	public void setIdlinhas(Linhas idlinhas) {
+		this.idlinhas = idlinhas;
+	}
+
+	public Pessoa getIdpessoa() {
+		return idpessoa;
+	}
+
+	public void setIdpessoa(Pessoa idpessoa) {
+		this.idpessoa = idpessoa;
+	}
 
 }
